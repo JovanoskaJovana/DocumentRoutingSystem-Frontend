c22ddca3f8f743562dd62ee68e000413bc4196e6
Jovana <jovana.jovanoska003@gmail.com>
2025-11-06 14:41:57 +0100

    feat(app): add main routing structure and integrate layout components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import './App.css'
-import {BrowserRouter, Routes, Route} from "react-router";
-import Login from "./ui/components/auth/Login";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router";
+import Login from "./ui/components/Login/Login";
+import Layout from './ui/components/Layout/Layout';
+import DocumentList from './ui/components/inbox/DocumentList';
+import DownloadList from './ui/components/Downloads/DownloadList';
+import DocumentDeptList from './ui/components/Department/DocumentDeptList';
+import DocumentHistoryList from './ui/components/History/DocumentHistoryList';
+import DocumentActionList from './ui/components/Actions/DocumentActionList.jsx';
+import ProtectedRoute from './ui/components/ProtectedRoute/ProtectedRoute';
 
 const App = () => {
     return (
         <BrowserRouter>
-            <Routes>
-              <Route path="/login" element={<Login/>}/>
+            <Routes>         
+                <Route path="/login" element={<Login/>}/> 
+
+                <Route element={<ProtectedRoute/>}>
+                    <Route path="/" element={<Layout />}>
+                        <Route index element={<DocumentList />} />
+                        <Route path="downloads" element={<DownloadList />} />
+                        <Route path="department" element={<DocumentDeptList />} />
+                        <Route path="history" element={<DocumentHistoryList/>} />
+                        <Route path="documents/:documentId/actions" element={<DocumentActionList/>} />
+                    </Route>
+                </Route>
+                
             </Routes>
         </BrowserRouter>
     );
